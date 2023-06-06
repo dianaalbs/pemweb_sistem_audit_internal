af005a17e7b3b1c47558927537c8e239042b655c
Aisyah <aisyahayya03@student.uns.ac.id>
2023-06-06 10:35:10 +0700

    tes

--- app/Models/Standar_Ruang_Lingkup.php
+++ app/Models/Standar_Ruang_Lingkup.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Standar_Ruang_Lingkup extends Model
 {
     use HasFactory;
+
+    protected $table = 'standar_ruang_lingkup';
+
+    protected $primarykey = 'id_standar_ruang_lingkup';
+
+    protected $timestamps = false;
+
+    protected $fillable = [
+        'id_setup_file',
+        'nama_ruang_lingkup',
+        'jenis_ruang_lingkup',
+        'deskripsi_ruang_lingkup',
+    ];
+
+    //Relation with model setup_file
+    public function setup_file()
+    {
+        return $this->belongsTo(setup_file::class, 'id_setup_file');
+    }
 }
