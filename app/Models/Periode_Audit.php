d3596f48890dd11ba4f165bcb058fab4bb2566c7
Aisyah <aisyahayya03@student.uns.ac.id>
2023-06-06 11:33:33 +0700

    tes

--- app/Models/Periode_Audit.php
+++ app/Models/Periode_Audit.php
@@ -5,6 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Periode_Audit
+ *
+ * @property int $id_periode_audit
+ * @property string $tanggal_awal_audit
+ * @property string $tanggal_akhir_audit
+ * @property string $no_sk_tugas_audit
+ * @property string $file_sk
+ * @property string $tanggal_sk
+ * @property string $ketua_spi
+ * @property string $nip_ketua_spi
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereFileSk($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereIdPeriodeAudit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereKetuaSpi($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereNipKetuaSpi($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereNoSkTugasAudit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereTanggalAkhirAudit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereTanggalAwalAudit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereTanggalSk($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Periode_Audit whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Periode_Audit extends Model
 {
     use HasFactory;
--- app/Models/Standar_Ruang_Lingkup.php
+++ app/Models/Standar_Ruang_Lingkup.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Standar_Ruang_Lingkup
+ *
+ * @property int $id_standar_ruang_lingkup
+ * @property int $id_setup_file
+ * @property string $nama_ruang_lingkup
+ * @property string $jenis_ruang_lingkup
+ * @property string $deskripsi_ruang_lingkup
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Setup_File $setup_file
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereDeskripsiRuangLingkup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereIdSetupFile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereIdStandarRuangLingkup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereJenisRuangLingkup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereNamaRuangLingkup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Standar_Ruang_Lingkup whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Standar_Ruang_Lingkup extends Model
 {
     use HasFactory;
