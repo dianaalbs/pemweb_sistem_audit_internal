91d938851d1ea209470aa7c0463160c5127d6066
Aisyah <aisyahayya03@student.uns.ac.id>
2023-06-06 10:22:44 +0700

    tes

--- app/Models/Auditee.php
+++ app/Models/Auditee.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Auditee extends Model
 {
     use HasFactory;
+
+    protected $table = 'auditee';
+
+    protected $primarykey = 'id_auditee';
+
+    protected $timestamps = false;
+
+    protected $fillable = [
+        'id_user',
+        'nama_auditee',
+        'sub_bagian',
+    ];
+
+    //Relation with model user()
+    public function user()
+    {
+        return $this->belongsTo(user::class, 'id_user');
+    }
 }
+
