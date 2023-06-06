9e63e029228d7404c8aba97f23d3685429fa5b1b
dianaalbs <dianaadilla@student.uns.ac.id>
2023-06-06 10:47:51 +0700

    update model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+    protected $table = 'admin';
+
+    protected $primarykey = 'id_admin';
+
+    protected $fillable = [
+        'id_user',
+        'nama'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    //Relation with model Users()
+    public function user()
+    {
+        return $this->belongsTo(Users::class, 'id_user');
+    }
 }
